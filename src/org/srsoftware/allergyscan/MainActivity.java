6cfbfc620d415e4e23387e5e62269593045dadce
srichter <srichter@55db7c28-97d7-418f-b348-6af10d321ed5>
2012-08-28 15:54:53 +0000

    recreated project
    git-svn-id: http://kommune10.dyndns.info:815/svn/AllergyScan@101 55db7c28-97d7-418f-b348-6af10d321ed5

---
+++ src/org/srsoftware/allergyscan/MainActivity.java
@@ -0,0 +1,54 @@
+package org.srsoftware.allergyscan;
+
+import com.example.allergyscan.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.telephony.TelephonyManager;
+import android.view.Menu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	protected static String deviceid = null;
+	protected Object allergenList = null;
+	
+		private void getDeviceId() {
+			TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+			deviceid = telephonyManager.getDeviceId();
+		}
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(getApplicationContext(), "onCreate", 5).show();
+        getDeviceId();
+        setContentView(R.layout.activity_main);
+    }
+    
+    @Override
+    protected void onStart() {    	
+    	super.onStart();
+    	allergenList=readAllergenList();
+    	Toast.makeText(getApplicationContext(), "onStart", 5).show();
+    }
+
+    private Object readAllergenList() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+    protected void onResume() {
+    	super.onResume();
+    	Toast.makeText(getApplicationContext(), "onResume", 5).show();
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
