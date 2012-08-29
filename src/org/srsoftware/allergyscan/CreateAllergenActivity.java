c18f716fe492492cf7098a9a4306b3b966222e44
srichter <srichter@55db7c28-97d7-418f-b348-6af10d321ed5>
2012-08-29 13:25:50 +0000

    prepared creation of new allergens
    git-svn-id: http://kommune10.dyndns.info:815/svn/AllergyScan@113 55db7c28-97d7-418f-b348-6af10d321ed5

--- src/org/srsoftware/allergyscan/CreateAllergenActivity.java
+++ src/org/srsoftware/allergyscan/CreateAllergenActivity.java
@@ -3,7 +3,11 @@ package org.srsoftware.allergyscan;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnKeyListener;
+import android.widget.AutoCompleteTextView;
 
 import com.example.allergyscan.R;
 
@@ -15,9 +19,28 @@ public class CreateAllergenActivity extends Activity {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "CreateAllergenActivity.onCreate");
         setContentView(R.layout.activity_create_allergen);
+        
+        final AutoCompleteTextView text=(AutoCompleteTextView)findViewById(R.id.autoCompleteTextView1);
+        OnKeyListener listener=new OnKeyListener() {
+					
+					@Override
+					public boolean onKey(View v, int keyCode, KeyEvent event) {
+						if (event.getAction()==KeyEvent.ACTION_UP && keyCode==KeyEvent.KEYCODE_ENTER){
+							storeAllergen(text.getText().toString().trim());
+						}
+						return false;
+					}
+				};
+				text.setOnKeyListener(listener);
     }
 
-    @Override
+    protected void storeAllergen(String allergen) {
+	    // TODO Auto-generated method stub
+			Log.d(TAG, "here, \""+allergen+"\" should be stored");
+
+    }
+
+		@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_create_allergen, menu);
         return true;
