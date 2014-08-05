2b5654650733f4d79ff590e000a081fbaf58d2f2
Stephan Richter <github@srsoftware.de>
2014-08-05 17:12:34 +0200

    prepared synchronization

--- src/com/srsoftware/allergyscan/SynchronizeActivity.java
+++ src/com/srsoftware/allergyscan/SynchronizeActivity.java
@@ -2,16 +2,34 @@ package com.srsoftware.allergyscan;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 import com.srsoftware.allergyscan.R;
 
-public class SynchronizeActivity extends Activity {
+public class SynchronizeActivity extends Activity implements OnClickListener {
 
-    @Override
+    private Button alwaysOkButton;
+		private Button onceOkButton;
+		private Button noButton;
+
+		@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_synchronize);
+        
+        alwaysOkButton=(Button)findViewById(R.id.alway_ok);
+        alwaysOkButton.setOnClickListener(this);
+        
+        onceOkButton=(Button)findViewById(R.id.once_ok);
+        onceOkButton.setOnClickListener(this);
+
+        noButton=(Button)findViewById(R.id.dont_sync);
+        noButton.setOnClickListener(this);
+
     }
 
     @Override
@@ -19,4 +37,16 @@ public class SynchronizeActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_synchronize, menu);
         return true;
     }
+
+		public void onClick(View v) {
+			if (v==alwaysOkButton){
+				Log.d("TEst", "ok");
+			}
+			if (v==onceOkButton){
+				Log.d("TEst", "ok once");
+			}
+			if (v==noButton){
+				Log.d("TEst", "not ok");
+			}
+		}
 }
