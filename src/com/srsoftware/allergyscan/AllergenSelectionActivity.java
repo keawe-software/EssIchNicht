8d0941a2a28505bafbeed0a7b1b20e5321e5e987
Stephan Richter <github@srsoftware.de>
2014-08-18 09:56:22 +0200

    added limitation: free version users can activate only to allergens

--- src/com/srsoftware/allergyscan/AllergenSelectionActivity.java
+++ src/com/srsoftware/allergyscan/AllergenSelectionActivity.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Vector;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,110 +21,123 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 public class AllergenSelectionActivity extends Activity implements OnClickListener {
-		protected static String TAG="AllergyScan";
-		private Button createButton,storeButton;
-		private ListView list;
-		private AllergenList availableAllergens;
-		private SharedPreferences settings;
-		private AllergyScanDatabase localDatabase;
-		
-		
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_allergen_selection);
+	protected static String TAG = "AllergyScan";
+	private Button createButton, storeButton;
+	private ListView list;
+	private AllergenList availableAllergens;
+	private SharedPreferences settings;
+	private AllergyScanDatabase localDatabase;
 
-        settings=getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE); // create settings handle
-        localDatabase=new AllergyScanDatabase(getApplicationContext(),settings); // create database handle
-        createButton=(Button)findViewById(R.id.createAllergenButton);
-        storeButton=(Button)findViewById(R.id.storeAllergenSelection);
-        list = (ListView)findViewById(R.id.listView1);
-        createButton.setOnClickListener(this);
-        storeButton.setOnClickListener(this);
-    }
-    
-    
-    @Override
-    protected void onResume() {
-    	super.onResume();
-      createListOfAllAllergens();
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_allergen_selection);
 
-    };
-    
-    private void startSynchronizeActivity() {
-			startActivity(new Intent(this,SynchronizeActivity.class)); // start the learning activity
-		}
+		settings = getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE); // create settings handle
+		localDatabase = new AllergyScanDatabase(getApplicationContext(), settings); // create database handle
+		createButton = (Button) findViewById(R.id.createAllergenButton);
+		storeButton = (Button) findViewById(R.id.storeAllergenSelection);
+		list = (ListView) findViewById(R.id.listView1);
+		createButton.setOnClickListener(this);
+		storeButton.setOnClickListener(this);
+	}
 
-    private void createListOfAllAllergens() {
-      availableAllergens=localDatabase.getAllAllergens();
-      if (availableAllergens==null || availableAllergens.isEmpty()){
-       	Toast.makeText(getApplicationContext(), R.string.no_allergens_in_database, Toast.LENGTH_LONG).show();
-       	startSynchronizeActivity();
-      } else {
-        list.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);      
-       	List<Allergen> allergens = availableAllergens.values(); // make string list
-       	ArrayAdapter<Allergen> adapter = new ArrayAdapter<Allergen>(this, android.R.layout.simple_list_item_multiple_choice,allergens);
-       	list.setAdapter(adapter);          	// Assign adapter to ListView
-       	int size=allergens.size();
-       	for (int i=0; i<size; i++){
-       		Object object = list.getItemAtPosition(i);
-       		if (object instanceof Allergen){
-       			if (((Allergen)object).active){
-       				list.setItemChecked(i, true);
-       			}
-         	}
-        }
-      }
-		}
+	@Override
+	protected void onResume() {
+		super.onResume();
+		createListOfAllAllergens();
 
-		@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_allergen_selection, menu);
-        return true;
-    }
+	};
 
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }    
+	private void startSynchronizeActivity() {
+		startActivity(new Intent(this, SynchronizeActivity.class)); // start the learning activity
+	}
 
-		public void onClick(View v) {
-			if (v==storeButton){
-				storeAllergenSelection();
-			}
-			if (v==createButton){
-				createNewAllergen();
+	private void createListOfAllAllergens() {
+		availableAllergens = localDatabase.getAllAllergens();
+		if (availableAllergens == null || availableAllergens.isEmpty()) {
+			Toast.makeText(getApplicationContext(), R.string.no_allergens_in_database, Toast.LENGTH_LONG).show();
+			startSynchronizeActivity();
+		} else {
+			list.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+			List<Allergen> allergens = availableAllergens.values(); // make string list
+			ArrayAdapter<Allergen> adapter = new ArrayAdapter<Allergen>(this, android.R.layout.simple_list_item_multiple_choice, allergens);
+			list.setAdapter(adapter); // Assign adapter to ListView
+			int size = allergens.size();
+			for (int i = 0; i < size; i++) {
+				Object object = list.getItemAtPosition(i);
+				if (object instanceof Allergen) {
+					if (((Allergen) object).active) {
+						list.setItemChecked(i, true);
+					}
+				}
 			}
 		}
+	}
 
-		private void storeAllergenSelection() {
-     	int size=list.getCount();
-     	Vector<Allergen> enabledAllergens=new Vector<Allergen>();
-     	for (int i=0; i<size; i++){
-     		Object object = list.getItemAtPosition(i);
-     		if (object instanceof Allergen){
-     			if (list.isItemChecked(i)){
-     				enabledAllergens.add((Allergen) object);
-     			}
-       	}
-      }
-     	while (enabledAllergens.size()>2){ // for the free version
-     		enabledAllergens.remove(0); // TODO: remove for full version
-     	}
-     	localDatabase.setEnabled(enabledAllergens);
-		  finish();
-		  startSynchronizeActivity();
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_allergen_selection, menu);
+		return true;
+	}
 
-
-		private void createNewAllergen() {
-			Intent intent=new Intent(this,CreateAllergenActivity.class);
-			startActivity(intent);			
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
 		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	public void onClick(View v) {
+		if (v == storeButton) {
+			storeAllergenSelection();
+		}
+		if (v == createButton) {
+			createNewAllergen();
+		}
+	}
+
+	private void storeAllergenSelection() {
+		int size = list.getCount();
+		Vector<Allergen> enabledAllergens = new Vector<Allergen>();
+		for (int i = 0; i < size; i++) {
+			Object object = list.getItemAtPosition(i);
+			if (object instanceof Allergen) {
+				if (list.isItemChecked(i)) {
+					enabledAllergens.add((Allergen) object);
+				}
+			}
+		}
+		if (!showFreeVersionHint(enabledAllergens)){
+			localDatabase.setEnabled(enabledAllergens);
+			finish();
+			startSynchronizeActivity();
+		}
+	}
+
+	private boolean showFreeVersionHint(Vector<Allergen> enabledAllergens) {
+		if (enabledAllergens.size() > 2) {
+			Log.d(TAG, "showFreeVersionHint");
+			AlertDialog ad = new AlertDialog.Builder(this).create();
+			ad.setCancelable(false); // disable Back button
+			ad.setMessage(getString(R.string.free_version_hint));
+			ad.setButton(AlertDialog.BUTTON_POSITIVE, getString(R.string.ok), new DialogInterface.OnClickListener() {
+
+				public void onClick(DialogInterface dialog, int which) {
+					dialog.dismiss();
+				}
+			});
+			ad.show();
+			return true;
+		}
+		return false;
+	}
+
+	private void createNewAllergen() {
+		Intent intent = new Intent(this, CreateAllergenActivity.class);
+		startActivity(intent);
+	}
 }
