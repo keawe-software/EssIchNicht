5881c612dacd1369628ebeea3235a01864e88033
Stephan Richter <github@srsoftware.de>
2014-08-29 02:44:59 +0200

    resolved issue #1

--- src/com/srsoftware/allergyscan/AllergenSelectionActivity.java
+++ src/com/srsoftware/allergyscan/AllergenSelectionActivity.java
@@ -92,11 +92,16 @@ public class AllergenSelectionActivity extends Activity implements OnClickListen
 			storeAllergenSelection();
 		}
 		if (v == createButton) {
+			storeAllergenSelection(false);
 			createNewAllergen();
 		}
 	}
 
 	private void storeAllergenSelection() {
+		storeAllergenSelection(true);
+	}
+
+	private void storeAllergenSelection(boolean updateAfter) {
 		int size = list.getCount();
 		Vector<Allergen> enabledAllergens = new Vector<Allergen>();
 		for (int i = 0; i < size; i++) {
@@ -109,8 +114,10 @@ public class AllergenSelectionActivity extends Activity implements OnClickListen
 		}
 		if (!showFreeVersionHint(enabledAllergens)){
 			localDatabase.setEnabled(enabledAllergens);
-			finish();
-			startSynchronizeActivity();
+			if (updateAfter){
+				finish();
+				startSynchronizeActivity();
+			}
 		}
 	}
 
